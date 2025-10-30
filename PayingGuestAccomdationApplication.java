2bb65318d592210fa73c6630f74b03ed3749dbb4
Pranjali Sharma <104714667+SharmaPranjali25@users.noreply.github.com>
2025-10-30 10:21:46 +0530

    Add files via upload

---
+++ PayingGuestAccomdationApplication.java
@@ -0,0 +1,13 @@
+package com.codewithpranjali.Paying_Guest_Accomdation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayingGuestAccomdationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayingGuestAccomdationApplication.class, args);
+	}
+
+}

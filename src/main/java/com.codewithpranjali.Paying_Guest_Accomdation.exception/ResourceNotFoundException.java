4e5e2327ac78dd7258df2a47b4967303ff096704
Pranjali Sharma <104714667+SharmaPranjali25@users.noreply.github.com>
2025-10-30 10:32:58 +0530

    Create ResourceNotFoundException.java

---
+++ src/main/java/com.codewithpranjali.Paying_Guest_Accomdation.exception/ResourceNotFoundException.java
@@ -0,0 +1,5 @@
+package com.codewithpranjali.Paying_Guest_Accomdation.exceotion;
+
+public class ResourceNotFoundException extends RuntimeException {
+public ResourceNotFoundException(String message) { super(message); }
+}

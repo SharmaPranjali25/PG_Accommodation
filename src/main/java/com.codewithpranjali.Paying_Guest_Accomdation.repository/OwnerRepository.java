bdd91237483c111e64bf9894a62b364d880c3737
Pranjali Sharma <104714667+SharmaPranjali25@users.noreply.github.com>
2025-10-30 10:33:50 +0530

    Create OwnerRepository.java

---
+++ src/main/java/com.codewithpranjali.Paying_Guest_Accomdation.repository/OwnerRepository.java
@@ -0,0 +1,6 @@
+package com.codewithpranjali.Paying_Guest_Accomdation.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.codewithpranjali.Paying_Guest_Accomdation.entites.Owner;
+public interface OwnerRepository extends JpaRepository<Owner, Long> {}
